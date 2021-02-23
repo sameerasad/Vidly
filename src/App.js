042d9171436697a781d31fd0db881205b18bdcce
sameerasad <ssameerkhan525@gmail.com>
2021-02-23 21:57:03 +0500

    adding  bootstrap classes dynamically

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Fragment } from "react";
 import Counter from "./components/counter";
 
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,16 +10,27 @@ export default function Counter() {
    
    const formatCount=()=>{
      return  count===0 ? "Zero":count
+
+     
    }
+
+   getBadgeClasses()
     return (
         <div>
            
            
-           <span className="badge m-2 badge-primary badge-size-20"> {formatCount()}</span>
+           <span style={{fontSize:20}} className={getBadgeClasses()} > {formatCount()}</span>
            <button className="btn btn-secondary btn-sm" onClick={()=>{setCount(count++)}} >Increament</button>
            <button className="btn btn-warning m-2 btn-sm" onClick={()=>{setCount(count--)}} >Decreament</button>
         </div>
     )
+
+    
+    function getBadgeClasses() {
+        let classes = "badge m-2 badge-"
+        classes += count === 0 ? "danger" : "primary"
+        return classes
+    }
 }
 
 
