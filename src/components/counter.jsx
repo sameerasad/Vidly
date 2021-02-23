d8da9d87b4935c006af6f2cbbb5f67ebdf91fbe5
sameerasad <ssameerkhan525@gmail.com>
2021-02-23 20:14:56 +0500

    setting attributes and adding bootstrap classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,21 @@ import React,{useState} from 'react'
 export default function Counter() {
 
     let [count, setCount] = useState(0)
-
+    
+    
+    
+   
+   
    const formatCount=()=>{
      return  count===0 ? "Zero":count
    }
     return (
         <div>
-           <span> {formatCount()}</span>
-           <button onClick={()=>{setCount(count++)}} >Increament</button>
-           <button onClick={()=>{setCount(count--)}} >Decreament</button>
+           
+           
+           <span className="badge m-2 badge-primary badge-size-20"> {formatCount()}</span>
+           <button className="btn btn-secondary btn-sm" onClick={()=>{setCount(count++)}} >Increament</button>
+           <button className="btn btn-warning m-2 btn-sm" onClick={()=>{setCount(count--)}} >Decreament</button>
         </div>
     )
 }
