60fa8350568f0b0f71b8b3a660e691fa1013ac53
sameerasad <ssameerkhan525@gmail.com>
2021-02-23 22:27:52 +0500

    rendering
     listing

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,23 @@
 import React,{useState} from 'react'
 
 export default function Counter() {
-
+    let tags=[{name:"tag1",id:1},{name:"tag2",id:2},{name:"tag3",id:3}]
+    
+    
     let [count, setCount] = useState(0)
     
-    
-    
-   
-   
-   const formatCount=()=>{
+      const formatCount=()=>{
      return  count===0 ? "Zero":count
 
      
    }
+   function getBadgeClasses() {
+    let classes = "badge m-2 badge-"
+    classes += count === 0 ? "danger" : "primary"
+    return classes
+}
 
-   getBadgeClasses()
+   
     return (
         <div>
            
@@ -22,15 +25,12 @@ export default function Counter() {
            <span style={{fontSize:20}} className={getBadgeClasses()} > {formatCount()}</span>
            <button className="btn btn-secondary btn-sm" onClick={()=>{setCount(count++)}} >Increament</button>
            <button className="btn btn-warning m-2 btn-sm" onClick={()=>{setCount(count--)}} >Decreament</button>
+           <ul>{tags.map(tag=><li key={tag.id}>{tag.name}</li>)}</ul>
         </div>
     )
 
     
-    function getBadgeClasses() {
-        let classes = "badge m-2 badge-"
-        classes += count === 0 ? "danger" : "primary"
-        return classes
-    }
+    
 }
 
 
