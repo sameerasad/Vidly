1c445e82b50ed0a57acaab65d068abc4773874fd
sameerasad <ssameerkhan525@gmail.com>
2021-02-24 19:35:01 +0500

    handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,52 @@
 import React,{useState} from 'react'
 
 export default function Counter() {
-    let tags=[]
+    let tags=[{name:"tag1",id:1},{name:"tag2",id:2},{name:"tag3",id:3}]
     
-   function renderTags(){
+    
+    let [count, setCount] = useState(0)
+    
+      const formatCount=()=>{
+     return  count===0 ? "Zero":count
+
+     
+   }
+   function getBadgeClasses() {
+    let classes = "badge m-2 badge-"
+    classes += count === 0 ? "danger" : "primary"
+    return classes
+}
+
+function renderTags(){
 
     if(tags.length===0)return <p> there is no tags</p>;
     return <ul>{tags.map(tag=><li key={tag.id}>{tag.name}</li>)}</ul>
 
 
    } 
-   
-   
+
+   function Increament () {
+     return setCount(count++)
+       
+
+       
+   }
+   function Decreament () {
+    return setCount(count--)
+      
+
+      
+  }
 
    
     return (
         <div>
-          { tags.length===0 && <p>there is no tag</p>}
+           
+           
+           <span style={{fontSize:20}} className={getBadgeClasses()} > {formatCount()}</span>
+           <button className="btn btn-secondary btn-sm" onClick={()=>{Increament()}} >Increament</button>
+           <button className="btn btn-warning m-2 btn-sm" onClick={()=>{Decreament()}} >Decreament</button>
+           {renderTags()}
         </div>
     )
 
