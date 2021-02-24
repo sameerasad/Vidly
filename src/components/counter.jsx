0cf4e1a3819ac3506bb4672d27b540cf742b7fa8
sameerasad <ssameerkhan525@gmail.com>
2021-02-24 18:11:26 +0500

    comditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,29 +3,22 @@ import React,{useState} from 'react'
 export default function Counter() {
     let tags=[{name:"tag1",id:1},{name:"tag2",id:2},{name:"tag3",id:3}]
     
-    
-    let [count, setCount] = useState(0)
-    
-      const formatCount=()=>{
-     return  count===0 ? "Zero":count
+   function renderTags(){
 
-     
-   }
-   function getBadgeClasses() {
-    let classes = "badge m-2 badge-"
-    classes += count === 0 ? "danger" : "primary"
-    return classes
-}
+    if(tags.length===0)return <p> there is no tags</p>;
+    return <ul>{tags.map(tag=><li key={tag.id}>{tag.name}</li>)}</ul>
+
+
+   } 
+   
+   
 
    
     return (
         <div>
-           
-           
-           <span style={{fontSize:20}} className={getBadgeClasses()} > {formatCount()}</span>
-           <button className="btn btn-secondary btn-sm" onClick={()=>{setCount(count++)}} >Increament</button>
-           <button className="btn btn-warning m-2 btn-sm" onClick={()=>{setCount(count--)}} >Decreament</button>
-           <ul>{tags.map(tag=><li key={tag.id}>{tag.name}</li>)}</ul>
+          {
+             renderTags() 
+          }
         </div>
     )
 
