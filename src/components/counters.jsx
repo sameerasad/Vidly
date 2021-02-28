af0f6de8f9f0985ae79102fa0ea2d82c40c3e171
sameerasad <ssameerkhan525@gmail.com>
2021-02-28 14:39:53 +0500

    make the counter componet controlled component to change the local state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,7 +2,7 @@ import React,{ useState} from 'react'
 import Counter from "./counter"
 
 export default function Counters() {
-const [counters, setCounters] = useState(
+let [counters, setCounters] = useState(
   [
   {id:1,value:1},
   {id:2,value:2},
@@ -15,6 +15,29 @@ const handleDelete=(counterId)=> {
   console.log("event handler called",counterId)
   const newCounters = counters.filter(c=>c.id!==counterId)
   return setCounters(newCounters)
+ 
+}
+
+const handleIncreament=(counter)=> {
+  counters = [...counters]
+  const index = counters.indexOf(counter)
+  counters[index]={...counter}
+  counters[index].value++;
+
+  setCounters(counters)
+ 
+}
+
+
+const handleDecreament=(counter)=> {
+   counters = [...counters]
+  const index = counters.indexOf(counter)
+  counters[index]={...counter}
+  counters[index].value--;
+
+  setCounters(counters)
+
+  
  
 }
    
@@ -30,7 +53,7 @@ const  handleReset=()=> {
    return (
     <div>
       <button  className="btn btn-primary btn-sm m-2" onClick={()=>{handleReset(counters)}}>Resent</button>
-      {counters.map(counter=><Counter key={counter.id} value={counter.value} onDelete={()=>handleDelete(counter.id)} ></Counter>)}  
+      {counters.map(counter=><Counter key={counter.id} value={counter.value} onDelete={()=>handleDelete(counter.id)} onIncreament={()=>handleIncreament(counter)}  onDecreament={()=>handleDecreament(counter)}></Counter>)}  
        
     </div>
 )
