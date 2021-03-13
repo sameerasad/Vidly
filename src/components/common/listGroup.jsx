183674225e4d7e2c00ad933a01e6b1f71fe09ed4
sameerasad <ssameerkhan525@gmail.com>
2021-03-13 21:12:05 +0500

    handle filtering select

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 function ListGroup(props) {
 
+    const handleSelect =genre=>{
+
+        console.log(genre)
+      }
+
     const {items,value,textProperty} = props
     return (
        <ul className="list-group">
-           {items.map(item=><li key={item[value]} className="list-group-item">{item[textProperty]}</li>)}
+           {items.map(item=><li onClick={()=>handleSelect(item)} key={item[value]} className="list-group-item">{item[textProperty]}    </li>)}
            
        </ul> 
     )
+    
 
 
 }
